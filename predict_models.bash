fd0fcc56436bbd518869a4518146698dd2ea7912
Kyle Mylonakis <kmylonakis@math.ucsb.edu>
2018-09-06 09:24:41 -0700

    added prefixes

--- predict_models.bash
+++ predict_models.bash
@@ -1,9 +1,11 @@
 # Predict script for the models
 
-python3 predict.py --load-path trained_models/UNet_conv
-python3 predict.py --load-path trained_models/UNet_dense
-python3 predict.py --load-path trained_models/UNet_res
+PREFIX=trained_models/batch_norm_off/
 
-python3 predict.py --load-path trained_models/AE_conv
-python3 predict.py --load-path trained_models/AE_dense
-python3 predict.py --load-path trained_models/AE_res
+python3 predict.py --load-path $PREFIX"UNet_conv"
+python3 predict.py --load-path $PREFIX"UNet_dense"
+python3 predict.py --load-path $PREFIX"UNet_res"
+
+python3 predict.py --load-path $PREFIX"AE_conv"
+python3 predict.py --load-path $PREFIX"AE_dense"
+python3 predict.py --load-path $PREFIX"AE_res"
--- train_models.bash
+++ train_models.bash
@@ -1,9 +1,11 @@
 # Training script for the various neural networks
+PREFIX=trained_models/batch_norm_off/
 
-python3 runexperiments.py --config samples/UNet_conv_config.json --save-dir trained_models/UNet_conv
-python3 runexperiments.py --config samples/UNet_dense_config.json --save-dir trained_models/UNet_dense
-python3 runexperiments.py --config samples/UNet_res_config.json --save-dir trained_models/UNet_res
 
-python3 runexperiments.py --config samples/AE_conv_config.json --save-dir trained_models/AE_conv
-python3 runexperiments.py --config samples/AE_dense_config.json --save-dir trained_models/AE_dense
-python3 runexperiments.py --config samples/AE_res_config.json --save-dir trained_models/AE_res
+python3 runexperiments.py --config samples/UNet_conv_config.json --save-dir $PREFIX"UNet_conv"
+python3 runexperiments.py --config samples/UNet_dense_config.json --save-dir $PREFIX"UNet_dense"
+python3 runexperiments.py --config samples/UNet_res_config.json --save-dir $PREFIX"UNet_res"
+
+python3 runexperiments.py --config samples/AE_conv_config.json --save-dir $PREFIX"AE_conv"
+python3 runexperiments.py --config samples/AE_dense_config.json --save-dir $PREFIX"AE_dense"
+python3 runexperiments.py --config samples/AE_res_config.json --save-dir $PREFIX"AE_res"
