ed8e2e61dcc61e7ac7b707de74baaf2c770fbd66
Jay <jayroberts91@gmail.com>
2018-10-17 14:32:33 -0700

    rm unnecessary scripts

--- prepare_data.bash
+++
@@ -1,8 +0,0 @@
-# A script to convert Jim's binary fortran data into a single numpy array
-
-#matlab -nodesktop -nodisplay -nojvm -r "open_fortran_binary"
-matlab -nodesktop -nodisplay -nojvm -r "multi_output_open_fortran_binary"
-python3 multi_output_convert_mat_to_numpy.py
-rm *.mat
-
-echo Numpy files generated
