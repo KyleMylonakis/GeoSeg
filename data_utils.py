42ff606ed229859b236279035b96e28ac317df3a
Jay <jayroberts91@gmail.com>
2018-10-28 09:13:12 -0700

    Finished 2layer ground truth input function. Parallelization didn't work so we should go back and look at this. For now it should do fine though.

--- data_utils.py
+++ data_utils.py
@@ -61,8 +61,9 @@ def ground_truth_1d_2layer_single_example(labels_tensor,output_shape=100):
         """
 
         wave_speeds = labels_tensor[:-1]
+        #print(wave_speeds)
         ratio = labels_tensor[-1]
-
+        #assert False
         assert  0.0 < ratio < 1.0, 'Ratio must be in (0,1) but got %f'%ratio
 
         wave_speeds = list(wave_speeds)
@@ -78,23 +79,76 @@ def ground_truth_1d_2layer_single_example(labels_tensor,output_shape=100):
 
         return ground_truth
 
-tests = np.array([[.1,.3,.5],
-                  [0.3,0.1,0.9],
-                  [1.0,2.1,1.2] ])
+def ground_truth_1d_2layer_single_example_packed(labels_tensor_output_shape):
+        labels_tensor,output_shape = labels_tensor_output_shape
+        return ground_truth_1d_2layer_single_example(labels_tensor,output_shape=output_shape)
 
-for i in range(3):
-        test = tests[i,:]
-        print(ground_truth_1d_2layer_single_example(test,output_shape=11))
-
-def ground_truth_1d_2layer(labels_tensor,output_shape=100):
+def ground_truth_1d_2layer(raw_labels,output_shape=100, num_cores = 4):
         """
-        Assumes labels_tensor is of the form (num_samples, c1, c2, ratio) corresponding
+        Assumes raw_labels is of the form [(c1, c2, ratio)] corresponding
         to the ground truth where the top 100*ration % of pixels are c1 and 
         the bottom 100*(1-ratio)% are c2. Returns an (num_samples, output_shape) long tensor 
         with a 1 for high speed and 0 for low speed. 
+
+        Parameters:
+        -----------
+                raw_labels: np.array of shape (num_samples,3). Each row should correspond to ground truth 
+                        labels (c1,c2,ratio) as described above.
+                output_shape: Desired shape of 1D output for ground truth.
+                num_cores: (Deprecated) Number of cores to use in parallel processing.
+        Returns:
+        --------
+                ground_truth tensor of size output_shape. Each row is a 1 or 0 depending on whether it 
+                came from the higher or lower wavespeed region respectively. 
         """
-
-        ground_truth = np.zeros((output_shape,), dtype = np.float32)
+        num_samples = raw_labels.shape[0]        
         
+        # TODO: Look into why parallezation didn't improve performance.
+        #data = zip(raw_labels,[output_shape]*num_samples)
+        #with mp.Pool(processes = num_cores) as pool:
+        #        ground_truth = pool.map(ground_truth_1d_2layer_single_example_packed,data)
 
-        return None
+        #pool.close()
+        #pool.join()
+        result = [0]*num_samples
+        for i in range(num_samples):
+                example = raw_labels[i]
+                result[i] = ground_truth_1d_2layer_single_example(example,output_shape=output_shape)
+        
+        result = np.array(result)
+        return result
+
+
+# Tests ground_truth_1d_2layer and times it. 
+# TODO: Remove when ready to commit to master. 
+#       Maybe put in a test script.
+"""
+num_samples = 100000
+np.random.seed(1)
+test = np.random.rand(num_samples,3)
+
+
+
+import time 
+
+
+t0 = time.time()
+z = ground_truth_1d_2layer(test,output_shape= 11)
+party = time.time() - t0 
+
+out_test = [0]*num_samples
+t0 = time.time()
+for i in range(num_samples):
+        out = test[i,...]
+        out_test[i] = ground_truth_1d_2layer_single_example(out,output_shape=11)
+out_test = np.array(out_test)
+
+normy = time.time() - t0
+
+msg = " \n z shape: {z.shape} \n out shape: {out_test.shape} \n parralell time: {party} \n normal time: {normy}"
+
+print(msg.format(z=z,out_test=out_test,party=party,normy=normy))
+
+print(np.linalg.norm(out_test-z))
+"""     
+
