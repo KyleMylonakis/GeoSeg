e960c7f9ce5731d2ea4a8eba53cad1c2d3f240ce
Jay <jayroberts91@gmail.com>
2018-10-28 08:25:20 -0700

    added ratio size check

--- data_utils.py
+++ data_utils.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import multiprocessing as mp 
 
 # TODO: Rewrite in a parallel or vectorized manner Map/Reduce formalism for example
 #       May have to unroll the loops or something, or think about cache performance here.
@@ -53,10 +54,16 @@ def ground_truth_1d_2layer_single_example(labels_tensor,output_shape=100):
         corresponding to the ground truth where the top 100*ration % of 
         pixels are c1 and the bottom 100*(1-ratio)% are c2. Returns an 
         output_shape long tensor with a 1 for high speed and 0 for low speed. 
+
+        Parameters:
+        -----------
+                labels_tensor: np.array of shape (3,)
         """
 
         wave_speeds = labels_tensor[:-1]
         ratio = labels_tensor[-1]
+
+        assert  0.0 < ratio < 1.0, 'Ratio must be in (0,1) but got %f'%ratio
 
         wave_speeds = list(wave_speeds)
 
@@ -70,6 +77,14 @@ def ground_truth_1d_2layer_single_example(labels_tensor,output_shape=100):
         ground_truth = [wave_speeds.index(x) for x in ground_truth]
 
         return ground_truth
+
+tests = np.array([[.1,.3,.5],
+                  [0.3,0.1,0.9],
+                  [1.0,2.1,1.2] ])
+
+for i in range(3):
+        test = tests[i,:]
+        print(ground_truth_1d_2layer_single_example(test,output_shape=11))
 
 def ground_truth_1d_2layer(labels_tensor,output_shape=100):
         """
